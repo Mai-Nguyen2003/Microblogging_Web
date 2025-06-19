2b03101cff07c5c5d6cb9642de93863cc5889c15
Thi Tuyet Mai Nguyen <mai3008.work@gmail.com>
2025-06-19 19:12:14 +0200

    add backend-services

---
+++ taDa_backend/scr/main/java/oth/ics/wtp/taDa_backend/TaDaBackendApplication.java
@@ -0,0 +1,13 @@
+package oth.ics.wtp.taDa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaDaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaDaBackendApplication.class, args);
+    }
+
+}
